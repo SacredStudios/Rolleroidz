55620e89c0d517ebd52cc63350d0ee8d831103a3
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2022-11-29 19:24:58 -0500

    Added debug room

--- Sk8troidz/Assets/Scripts/EyeColor.cs
+++ Sk8troidz/Assets/Scripts/EyeColor.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class EyeColor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
         
     }
 
