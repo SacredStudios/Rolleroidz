08b95cd40ba3f9ed3f5c20646509ca74c7b60cd1
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-02-20 14:04:07 -0500

    butt

--- Sk8troidz/Assets/Scripts/MenuController.cs
+++ Sk8troidz/Assets/Scripts/MenuController.cs
@@ -1,48 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] GameObject transition;
-    [SerializeField] float trans_speed;
-    [SerializeField] float max_trans_speed;
     [SerializeField] Vector3 velocity;
     [SerializeField] Vector3 target_pos;
     [SerializeField] Vector3 start_pos;
-    [SerializeField] float smooth_time;
-    [SerializeField] bool going_down;
-    public void Transition_Up()
-    {
-       // going_down = false;
 
-    }
-    void Transition_Down()
-    {
-        going_down = true;
+    [SerializeField] GameObject StartBtn;
+    [SerializeField] GameObject Menu_Skatroid;
+    [SerializeField] GameObject StartMenu;
 
-    }
-    private void FixedUpdate()
-    {
-        if(going_down)
-        {
-            transition.transform.position = Vector3.SmoothDamp(transition.transform.position, new Vector3(transition.transform.position.x,target_pos.y,
-                transition.transform.position.z), ref velocity, smooth_time, max_trans_speed);
 
-        }
-        /* else
-         {
-             transition.transform.position = Vector3.SmoothDamp(transition.transform.position, start_pos, ref velocity, smooth_time, max_trans_speed);
-         } */
-    }
-    // Update is called once per frame
     public void ShowMainMenu()
     {
-        Transition_Down();
-        Invoke("Transition_Up",2f);
+        StartBtn.GetComponent<Button>().enabled = false;
+        StartCoroutine(Transition_Down());
+     
+        Invoke("HideStartMenu", 1f);
+        
     }
-    private void Start()
+  void HideStartMenu()
     {
-        start_pos = transition.transform.position;  
+        StartMenu.SetActive(false);
+        Menu_Skatroid.SetActive(true);
+    }
+
+    IEnumerator Transition_Down()
+    {
+        transition.transform.position = start_pos;
+        while (transition.transform.position.y > target_pos.y)
+        {
+            transition.transform.position -= velocity;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(Transition_Up());
+
+    }
+    IEnumerator Transition_Up()
+    {
+        transition.transform.position = target_pos;
+        while (transition.transform.position.y < start_pos.y)
+        {
+            transition.transform.position += velocity;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(2f);
+
+
+
     }
 }
