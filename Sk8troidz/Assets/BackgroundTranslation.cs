e9793fbb07ff573b5df0810831cdde316541a8ac
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-02-20 00:33:37 -0500

    Added background

---
+++ Sk8troidz/Assets/BackgroundTranslation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTranslation : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 starting_loc;
+    [SerializeField] Vector3 ending_loc;
+
+
+    void FixedUpdate()
+    {
+        this.transform.position += new Vector3(speed,0,0);
+        if(transform.position.x > ending_loc.x)
+        {
+            transform.position = starting_loc;
+        }
+    }
+    private void Start()
+    {
+        Debug.Log(this.transform.position); 
+    }
+}
