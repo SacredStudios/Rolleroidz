9844ff413b6689c2e308c96532ae9f2daac15bed
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-03-12 18:04:12 -0400

    spawn

--- Sk8troidz/Assets/Scripts/Respawn.cs
+++ Sk8troidz/Assets/Scripts/Respawn.cs
@@ -12,7 +12,6 @@ public class Respawn : MonoBehaviour
     [SerializeField] GameObject death_head;
     [SerializeField] GameObject point;
     [SerializeField] PointTally pt;
-    [SerializeField] PhotonView pv;
     public void Death()
     {
 
@@ -20,14 +19,14 @@ public class Respawn : MonoBehaviour
 
         GameObject death_anim_clone = Instantiate(death_anim, player.transform.position, Quaternion.identity);
         death_anim_clone.SetActive(true);
-        if (pv.IsMine) {
+        if (PhotonNetwork.IsMasterClient) {
              GameObject point_clone = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
              point_clone.SetActive(true);
              PhotonNetwork.LocalPlayer.AddScore(-1);
         }
 
         for (int i = 0; i < pt.points/2; i ++) {
-            if (pv.IsMine)
+            if (PhotonNetwork.IsMasterClient)
             {
                 GameObject point_clone2 = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
                 point_clone2.SetActive(true);
