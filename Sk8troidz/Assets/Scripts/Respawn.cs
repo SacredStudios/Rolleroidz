d6fe6fe6e8ce794b35ad1b1f8ee5ae3816cffd16
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-03-12 18:00:06 -0400

    Update Respawn.cs

--- Sk8troidz/Assets/Scripts/Respawn.cs
+++ Sk8troidz/Assets/Scripts/Respawn.cs
@@ -12,23 +12,27 @@ public class Respawn : MonoBehaviour
     [SerializeField] GameObject death_head;
     [SerializeField] GameObject point;
     [SerializeField] PointTally pt;
-
+    [SerializeField] PhotonView pv;
     public void Death()
-{
-        
+    {
+
         player.SetActive(false);
-        
+
         GameObject death_anim_clone = Instantiate(death_anim, player.transform.position, Quaternion.identity);
         death_anim_clone.SetActive(true);
-        
-            GameObject point_clone = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
-            point_clone.SetActive(true);
-            PhotonNetwork.LocalPlayer.AddScore(-1);
+        if (pv.IsMine) {
+             GameObject point_clone = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
+             point_clone.SetActive(true);
+             PhotonNetwork.LocalPlayer.AddScore(-1);
+        }
 
         for (int i = 0; i < pt.points/2; i ++) {
-            GameObject point_clone2 = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
-            point_clone2.SetActive(true);
-            PhotonNetwork.LocalPlayer.AddScore(-1);
+            if (pv.IsMine)
+            {
+                GameObject point_clone2 = PhotonNetwork.Instantiate(point.name, death_anim_clone.transform.position, Quaternion.identity);
+                point_clone2.SetActive(true);
+                PhotonNetwork.LocalPlayer.AddScore(-1);
+            }
         }
         
 
