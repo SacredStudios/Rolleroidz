3d7d873fc586bcd2af95a83c18054efd0e8372b1
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-03-12 23:24:54 -0400

    a

--- Sk8troidz/Assets/Scripts/LookAtCamera.cs
+++ Sk8troidz/Assets/Scripts/LookAtCamera.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-public class LookAtCamera : MonoBehaviour
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+public class LookAtCamera : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject canvas;
     [SerializeField] GameObject Player_Assets;
     [SerializeField] PhotonView pv;
-    [SerializeField] GameObject Player_Name;
+    [SerializeField] Text player_name;
+    [SerializeField] Text score;
     public static Camera cam;
     
 
@@ -16,19 +19,32 @@ public class LookAtCamera : MonoBehaviour
     {
         if(pv.IsMine)
         {
-            Player_Name.GetComponent<Text>().text = PhotonNetwork.NickName;
+            player_name.text = PhotonNetwork.NickName;
             canvas.SetActive(false);
             cam = Player_Assets.GetComponentInChildren<Camera>();
           
         }
         else
         {
-            Player_Name.GetComponent<Text>().text = pv.Owner.NickName;
+            player_name.text = pv.Owner.NickName;
             
             
         }
     }
-
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        score.text = "" + PhotonNetwork.LocalPlayer.GetScore();
+        if (PhotonNetwork.LocalPlayer.GetPhotonTeam().Code == 1)
+        {
+            score.color = new Color(155, 0, 0);
+            player_name.color = new Color(255, 0, 0);
+        }
+        if (PhotonNetwork.LocalPlayer.GetPhotonTeam().Code == 2)
+        {
+            score.color = new Color(155, 175, 175);
+            player_name.color = new Color(255, 175, 175);
+        }
+    }
     // Update is called once per frame
     void LateUpdate()
     {
---
+++ Sk8troidz/Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Photon.Pun.UtilityScripts;
+public class UIManager : MonoBehaviourPunCallbacks
+{ //Info about nicknames can be found in lookatcamera script --Probably should move it here
+    [SerializeField] Text score;
+    [SerializeField] Text nickname;
+   
+}
