45700647b5aabb8c147522e0850b84e54e6616b6
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-05-07 22:08:34 -0400

    Update Game_Manager.cs

--- Sk8troidz/Assets/Scripts/Game_Manager.cs
+++ Sk8troidz/Assets/Scripts/Game_Manager.cs
@@ -40,7 +40,6 @@ public class Game_Manager : MonoBehaviourPunCallbacks
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = false;
-        PhotonNetwork.LocalPlayer.SwitchTeam(9);
         weapon_list = GameObject.Find("WeaponList"); //I know gameobject.find is bad. Do you have any better ideas?
 
     }
@@ -74,7 +73,7 @@ public class Game_Manager : MonoBehaviourPunCallbacks
     { 
         foreach (Player player_temp in PhotonNetwork.PlayerList)
         {
-            yield return new WaitUntil(() => player_temp.GetPhotonTeam() != null && player_temp.GetPhotonTeam().Code != 9);
+            yield return new WaitUntil(() => player_temp.GetPhotonTeam() != null);
         }
         yield return new WaitUntil(() => temp1 != 9999 && temp2 != 9999);
         if (temp1 < temp2) //get team count from master client
@@ -182,6 +181,7 @@ public class Game_Manager : MonoBehaviourPunCallbacks
     }
     public override void OnLeftRoom()
     {
+        PhotonNetwork.LocalPlayer.LeaveCurrentTeam();
         PhotonNetwork.OfflineMode = true;
         PhotonNetwork.OfflineMode = false;
         PhotonNetwork.LoadLevel("StartingScene");
