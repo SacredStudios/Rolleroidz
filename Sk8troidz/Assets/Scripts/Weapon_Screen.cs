1d24e73e52db3042b9d9584ac8f4a89709cddcae
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-05-15 17:24:26 -0400

    weapon panel is working

--- Sk8troidz/Assets/Scripts/Weapon_Screen.cs
+++ Sk8troidz/Assets/Scripts/Weapon_Screen.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 
 public class Weapon_Screen : MonoBehaviour
 {
-    [SerializeField] GameObject Weapon_Btn;
+    [SerializeField] GameObject weapon_btn;
+    [SerializeField] GameObject weapon_panel;
     void Start()
     {
         GameObject weapon_list = GameObject.Find("WeaponList");
         foreach (Weapon w in weapon_list.GetComponent<Weapon_List>().my_weapon_list)
         {
-            GameObject btn = Instantiate(Weapon_Btn,this.gameObject.transform);
-            btn.SetActive(true);
-            Debug.Log("instantiatingbtn");
-            btn.GetComponent<Weapon_Btn>().weapon = w;
-            btn.GetComponent<Weapon_Btn>().list = weapon_list.GetComponent<Weapon_List>();
-            btn.GetComponentInChildren<RawImage>().texture = w.icon;
+            for (int i = 0; i < 10; i++) {
+                GameObject btn = Instantiate(weapon_btn, weapon_panel.transform);
+                btn.SetActive(true);
+                Debug.Log("instantiatingbtn");
+                btn.GetComponent<Weapon_Btn>().weapon = w;
+                btn.GetComponent<Weapon_Btn>().list = weapon_list.GetComponent<Weapon_List>();
+                btn.GetComponentInChildren<RawImage>().texture = w.icon;
+                weapon_panel.transform.position = new Vector3(weapon_panel.transform.position.x, btn.transform.position.y, weapon_panel.transform.position.z);
+            }
             
         }
         }
