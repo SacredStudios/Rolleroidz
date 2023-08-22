dd4ac2180a3f10ca18660186d20076c1e0961463
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2023-08-21 23:43:06 -0400

    trying to sync ragdolls

--- Sk8troidz/Assets/Scripts/Ragdoll.cs
+++ Sk8troidz/Assets/Scripts/Ragdoll.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Ragdoll : MonoBehaviour
+using Photon.Pun;
+public class Ragdoll : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
-    [SerializeField] List<Collider> colliders;
+    [SerializeField] public List<Collider> colliders;
     PlayerMovement pm;
     public static bool is_Ragdoll;
     [SerializeField] Rigidbody rb;
+    [SerializeField] PhotonView pv;
     private void Start()
     {
         pm = GetComponent<PlayerMovement>();
@@ -17,17 +18,29 @@ public class Ragdoll : MonoBehaviour
     // Update is called once per frame
     public void ActivateRagdolls ()
     {
-        GetComponent<Animator>().enabled = false;
+        if(pv.IsMine)
+        {
+            int id = gameObject.GetComponent<PhotonView>().ViewID;
+            pv.RPC("SyncRagdoll", RpcTarget.All, id);
+        }
+        
         pm.enabled = false;
         GetComponent<Weapon_Handler>().weapon = null;
         is_Ragdoll = true;
         rb.velocity = new Vector3(0, 0, 0);
-        Invoke("DeactivateRagdolls", 3f);
-        foreach (Collider collider in colliders)
+       // Invoke("DeactivateRagdolls", 3f);
+       
+    }
+    [PunRPC] void SyncRagdoll(int id)
+    {
+       GameObject player = PhotonView.Find(id).gameObject;
+       player.GetComponent<Animator>().enabled = false;
+        foreach (Collider collider in player.GetComponent<Ragdoll>().colliders)
         {
             collider.enabled = true;
             collider.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         }
+
     }
     public void DeactivateRagdolls()
     {
--- Sk8troidz/Assets/Scripts/Trick_System.cs
+++ Sk8troidz/Assets/Scripts/Trick_System.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using Photon.Pun;
+
 
 public class Trick_System : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
