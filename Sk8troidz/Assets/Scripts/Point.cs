c8d3199645755a6f4c9b2d8e161af6c272f19187
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2024-01-19 16:12:42 -0500

    Update Point.cs

--- Sk8troidz/Assets/Scripts/Point.cs
+++ Sk8troidz/Assets/Scripts/Point.cs
@@ -29,10 +29,7 @@ public class Point : MonoBehaviour
 
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
-        else
-        {
-            Debug.Log("none");
-        }
+       
     }
     void OnCollisionEnter(Collision collider)
     {
