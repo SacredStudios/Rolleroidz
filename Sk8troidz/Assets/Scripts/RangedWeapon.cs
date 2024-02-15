95a841e5d15b0d6090f8de597b0c53e8649bdc95
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2024-02-14 22:07:41 -0500

    Update RangedWeapon.cs

--- Sk8troidz/Assets/Scripts/RangedWeapon.cs
+++ Sk8troidz/Assets/Scripts/RangedWeapon.cs
@@ -3,68 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
-
 [CreateAssetMenu(fileName = "new_weapon", menuName = "Scripts/RangedWeapon", order = 1)]
 public class RangedWeapon : Weapon
 {
     public GameObject particle_trail;
     public GameObject particle_explosion;
     public GameObject impact_explosion;
-    public float explosionRadius = 5f; // The radius of the explosion effect
+
+
+
+    // MAKE GAME LIKE NSMBDS MULTIPLAYER
 
     public override void Shoot(GameObject parent, GameObject particle_pos, GameObject explosion_pos)
     {
+        //  Debug.DrawRay(parent.transform.position, particle_pos.transform.up * range, Color.green); //chage this to capsulecast
+
+
+
         Ray ray = new Ray(parent.transform.position, particle_pos.transform.up);
-        RaycastHit hit;
-        bool hasHit = Physics.SphereCast(ray, explosionRadius, out hit, range);
-
+        RaycastHit hit = new RaycastHit();
         PhotonNetwork.Instantiate(particle_trail.name, particle_pos.transform.position, particle_pos.transform.rotation);
+        Instantiate(particle_explosion, explosion_pos.transform.position, particle_pos.transform.rotation);
 
-        if (hasHit)
+        if (Physics.Raycast(ray, out hit)) //Target Acquired
         {
-            Instantiate(particle_explosion, explosion_pos.transform.position, particle_pos.transform.rotation);
-            Vector3 explosionPoint = hit.point;
-            Collider[] colliders = Physics.OverlapSphere(explosionPoint, explosionRadius);
 
-            foreach (var hitCollider in colliders)
+
+            if (hit.distance <= range)
             {
-                if (hitCollider.tag == "Player" && hitCollider.GetComponent<PhotonView>().Owner.GetPhotonTeam() != PhotonNetwork.LocalPlayer.GetPhotonTeam())
+                //Debug.Log(hit.collider.tag);
+                //  Debug.Log(hit.collider.GetComponent<PhotonView>().Owner.GetPhotonTeam() + " + " + PhotonNetwork.LocalPlayer.GetPhotonTeam());
+                PhotonNetwork.Instantiate(impact_explosion.name, hit.point, Quaternion.identity);
+                //"if()" is a good name of a book
+                if ((hit.collider.tag == "Player") && hit.collider.GetComponent<PhotonView>().Owner.GetPhotonTeam() != PhotonNetwork.LocalPlayer.GetPhotonTeam())
                 {
-                    ApplyDamage(hitCollider.gameObject, hitCollider.transform);
+                    Player_Health ph = hit.collider.GetComponent<Player_Health>();
+                    if (ph != null)
+                    {
+                        if (ph.current_health > 0)
+                        {
+                            Debug.Log("hit");
+                            if (ph.current_health - damage <= 0)
+                            {
+                                Debug.Log("it works");
+                                SpawnCoin(hit.collider.gameObject, hit.transform);
+                                ph.PlayerLastHit(pv.ViewID);
+                                parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
+
+                            }
+                            ph.Remove_Health(damage);
+                        }
+
+                    }
+                    else
+                    {
+                        Debug.Log("hit");
+                    }
+                    //call function from individual joints
+                    // Debug.Log(hit.collider.gameObject.GetComponent<PhotonView>().Owner.GetPhotonTeam() + "+" + pv.Owner.GetPhotonTeam());
+
+                }
+                else if (hit.collider.tag == "Player_Head" && hit.collider.GetComponentInParent<PhotonView>().Owner.GetPhotonTeam() != PhotonNetwork.LocalPlayer.GetPhotonTeam())
+                {
+                    Player_Health ph = hit.collider.GetComponentInParent<Player_Health>();
+                    if (ph != null)
+                    {
+                        if (ph.current_health > 0)
+                        {
+                            Debug.Log("headshot");
+                            if (ph.current_health - (damage * 1.5) <= 0)
+                            {
+                                SpawnCoin(hit.transform.parent.gameObject, hit.transform);
+                                Debug.Log("it works");
+                                ph.PlayerLastHit(pv.ViewID);
+                                parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
+
+                            }
+                            ph.Remove_Health(damage * 1.5f);
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("hit");
+                    }
+                    //Debug.Log("HeadShot");
+
                 }
             }
-
-            PhotonNetwork.Instantiate(impact_explosion.name, hit.point, Quaternion.identity);
         }
+
     }
-
-    private void ApplyDamage(GameObject target, Transform hitTransform)
-    {
-        Player_Health ph = target.GetComponent<Player_Health>();
-        if (ph != null && ph.current_health > 0)
-        {
-            float damageToApply = CalculateDamage(target.transform.position, hitTransform.position);
-            if (ph.current_health - damageToApply <= 0)
-            {
-                SpawnCoin(target, hitTransform);
-                ph.PlayerLastHit(pv.ViewID);
-                parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
-            }
-            ph.Remove_Health(damageToApply);
-        }
-    }
-
-    float CalculateDamage(Vector3 targetPosition, Vector3 explosionPosition)
-    {
-        // Simple distance-based damage calculation
-        float distance = Vector3.Distance(targetPosition, explosionPosition);
-        float damage = Mathf.Max(0, damage - distance); // Decrease damage with distance
-        return damage;
-    }
-
-  
-
-void SpawnCoin(GameObject dead_player, Transform trans)
+    void SpawnCoin(GameObject dead_player, Transform trans)
     {
         Debug.Log(player.GetComponent<PhotonView>().Owner.NickName);
         Debug.Log(dead_player.GetComponent<PhotonView>().Owner.NickName);
