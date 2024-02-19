f835342844655d759160fe85e0157d66a4aed752
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2024-02-19 13:42:03 -0500

    save weapons

--- Sk8troidz/Assets/Scripts/Weapon_List.cs
+++ Sk8troidz/Assets/Scripts/Weapon_List.cs
@@ -27,8 +27,25 @@ public class Weapon_List : MonoBehaviour
         sk8troid_menu = GameObject.Find("Sk8troid(MENUVERSION)");
 
         curr_weapon = my_weapon_list[0];
+        if(PlayerPrefs.HasKey("weapon"))
+        {
+            LoadWeapon(PlayerPrefs.GetString("weapon"));
+        }
+        
         //curr_weapon = all_weapon_list[Random.Range(0,2)]; //be sure to comment this out
         ChangeWeapon(curr_weapon);
+    }
+    public void LoadWeapon(string name)
+    {
+        foreach (Weapon weapon in my_weapon_list) //would be better if I used a hashmap
+        {
+
+            if (weapon.name == name)
+            {
+                ChangeWeapon(weapon);
+            }
+        }
+
     }
     Weapon GetCurrentWeapon() //will be used on startup to get player's last used weapon
     {
@@ -49,5 +66,7 @@ public class Weapon_List : MonoBehaviour
         range_slider.GetComponent<Slider>().value = weapon.range;
         speed_slider.GetComponent<Slider>().value = Mathf.Round((1 / weapon.weapon_delay)*10.0f)*0.1f; ; //this displays value to 10th decimal point
         attackcost_slider.GetComponent<Slider>().value = weapon.attack_cost;
+        PlayerPrefs.SetString("weapon", weapon.name);
+        PlayerPrefs.Save();
     }
 }
