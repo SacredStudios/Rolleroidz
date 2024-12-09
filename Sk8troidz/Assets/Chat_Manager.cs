9090d7304dff1897517f6b35212eef2e86c86956
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2024-12-08 20:13:30 -0500

    my mac is takin too long

---
+++ Sk8troidz/Assets/Chat_Manager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Photon.Chat;
+
+public class Chat_Manager : MonoBehaviour
+{
+    ChatClient chatClient;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
