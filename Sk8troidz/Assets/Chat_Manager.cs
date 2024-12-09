f7cb3f2ce5b20432f012042350a1fd6e321cef36
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2024-12-08 20:32:47 -0500

    getting started

--- Sk8troidz/Assets/Chat_Manager.cs
+++ Sk8troidz/Assets/Chat_Manager.cs
@@ -1,17 +1,76 @@
 using UnityEngine;
 using Photon.Chat;
+using Photon.Pun;
+using ExitGames.Client.Photon;
 
-public class Chat_Manager : MonoBehaviour
+public class Chat_Manager : MonoBehaviour, IChatClientListener
 {
     ChatClient chatClient;
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnConnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDisconnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+    [SerializeField] string userId;
     void Start()
     {
-        
+        chatClient = new ChatClient(this);
+        chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(userId));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        chatClient.Service();
     }
 }
