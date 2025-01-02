0c5ea34267352d9d2057c600f06a8c3c0bcc4fd3
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2025-01-01 21:05:13 -0700

    more changes

--- Sk8troidz/Assets/Scripts/Weapon/RangedWeapon.cs
+++ Sk8troidz/Assets/Scripts/Weapon/RangedWeapon.cs
@@ -17,7 +17,7 @@ public class RangedWeapon : Weapon
 
 
 
-        float radius = 0.65f;
+        float radius = 0.7f;
         Ray ray = new Ray(parent.transform.position, particle_pos.transform.up); //-new Vector3(radius, 0, 0), 
         RaycastHit hit = new RaycastHit();
         PhotonNetwork.Instantiate(particle_trail.name, particle_pos.transform.position, particle_pos.transform.rotation);
