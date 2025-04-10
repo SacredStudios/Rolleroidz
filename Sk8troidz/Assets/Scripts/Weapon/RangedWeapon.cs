44cac0755346e3e1a7cfbb47f29e0ce2243543f1
SacredStudios <97319486+SacredStudios@users.noreply.github.com>
2025-04-10 16:36:15 -0400

    new weapon logic

--- Sk8troidz/Assets/Scripts/Weapon/RangedWeapon.cs
+++ Sk8troidz/Assets/Scripts/Weapon/RangedWeapon.cs
@@ -13,162 +13,149 @@ public class RangedWeapon : Weapon
 
     public override void Shoot(GameObject parent, GameObject particle_pos, GameObject explosion_pos)
     {
-        //  Debug.DrawRay(parent.transform.position, particle_pos.transform.up * range, Color.green); //chage this to capsulecast
-
-
-
         float radius = 0.3f;
-        Ray ray = new Ray(parent.transform.position, particle_pos.transform.up); //-new Vector3(radius, 0, 0), 
-        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(parent.transform.position, particle_pos.transform.up);
         PhotonNetwork.Instantiate(particle_trail.name, particle_pos.transform.position, particle_pos.transform.rotation);
         Instantiate(particle_explosion, explosion_pos.transform.position, particle_pos.transform.rotation);
 
-        Ray distance = new Ray(player.transform.position+new Vector3(radius/2, 0, 0), particle_pos.transform.up);
-
+        // This ray is used to check min_distance from player's side
+        Ray distance = new Ray(player.transform.position + new Vector3(radius / 2, 0, 0), particle_pos.transform.up);
         if (Physics.Raycast(distance, min_distance))
         {
-            //TODO: fix this by shooting from center of player
+            // If min_distance is too short, do nothing or fix your "shoot from center" logic
         }
         else
         {
+            // 1) Attempt SphereCastIgnoreKinematic with the "normal" radius
+            RaycastHit? possibleHit = SphereCastIgnoreKinematic(ray, radius, range);
 
-            if (Physics.SphereCast(ray, radius, out hit, range)) //Target Acquired
+            if (possibleHit.HasValue)
             {
-
-
+                RaycastHit hit = possibleHit.Value;
                 if (hit.distance <= range)
                 {
-                    //  Debug.Log(hit.collider.GetComponent<PhotonView>().Owner.GetPhotonTeam() + " + " + PhotonNetwork.LocalPlayer.GetPhotonTeam());
                     PhotonNetwork.Instantiate(impact_explosion.name, hit.point, Quaternion.identity);
-                    //"if()" is a good name of a book
-                    if ((hit.collider.tag == "AI_Player" || hit.collider.tag == "Player") && hit.collider.GetComponent<Team_Handler>().GetTeam() != player.GetComponent<Team_Handler>().GetTeam())
+
+                    // Example damage/target logic
+                    if ((hit.collider.CompareTag("AI_Player") || hit.collider.CompareTag("Player")) &&
+                        hit.collider.GetComponent<Team_Handler>().GetTeam() != player.GetComponent<Team_Handler>().GetTeam())
                     {
                         Player_Health ph = hit.collider.GetComponent<Player_Health>();
-                        if (ph != null)
+                        if (ph != null && ph.current_health > 0)
                         {
-                            if (ph.current_health > 0)
+                            if (ph.current_health - damage <= 0)
                             {
-
-                                if (ph.current_health - damage <= 0)
-                                {
-                                    PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
-                                    Transform oldpos = hit.collider.transform;
-                                    hit.collider.transform.position = new Vector3(9999, 9999, 9999);
-                                    SpawnCoin(hit.transform.gameObject, hit.point);
-                                    //ph.PlayerLastHit(pv.ViewID); //base this off of the gameObject, not ID
-                                    parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
-
-
-                                }
-                                ph.Remove_Health(damage);
+                                PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
+                                hit.collider.transform.position = new Vector3(9999, 9999, 9999);
+                                SpawnCoin(hit.transform.gameObject, hit.point);
+                                parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
                             }
-
+                            ph.Remove_Health(damage);
                         }
-                        else
-                        {
-
-                        }
-
                     }
-                    else if ((hit.collider.tag == "AI_Head" || hit.collider.tag == "Player_Head") && hit.collider.GetComponentInParent<Team_Handler>().GetTeam() != player.GetComponent<Team_Handler>().GetTeam())
+                    else if ((hit.collider.CompareTag("AI_Head") || hit.collider.CompareTag("Player_Head")) &&
+                             hit.collider.GetComponentInParent<Team_Handler>().GetTeam() != parent.GetComponent<Team_Handler>().GetTeam())
                     {
                         Player_Health ph = hit.collider.GetComponentInParent<Player_Health>();
-                        if (ph != null)
+                        if (ph != null && ph.current_health > 0)
                         {
-                            if (ph.current_health > 0)
+                            if (ph.current_health - (damage * 1.5f) <= 0)
                             {
-                                if (ph.current_health - (damage * 1.5) <= 0)
-                                {
-                                    PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
-                                    Transform oldpos = hit.collider.transform;
-                                    hit.collider.transform.position = new Vector3(9999, 9999, 9999);
-                                    SpawnCoin(hit.transform.parent.gameObject, hit.point);
-                                    //ph.PlayerLastHit(pv.ViewID);
-                                    parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
-
-                                }
-                                ph.Remove_Health(damage * 1.5f);
+                                PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
+                                hit.collider.transform.position = new Vector3(9999, 9999, 9999);
+                                SpawnCoin(hit.transform.parent.gameObject, hit.point);
+                                parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
                             }
+                            ph.Remove_Health(damage * 1.5f);
                         }
-                        else
-                        {
-                            Debug.Log("too short");
-                        }
-
-
                     }
-
                 }
             }
-            else if (Physics.SphereCast(ray, radius * 2, out hit, range)) //Target Acquired
+            else
             {
+                // 2) If the first cast didn't hit a valid non-kinematic object, try with bigger radius
+                RaycastHit? secondHit = SphereCastIgnoreKinematic(ray, radius * 2, range);
 
-
-                if (hit.distance <= range)
+                if (secondHit.HasValue)
                 {
-                    //  Debug.Log(hit.collider.GetComponent<PhotonView>().Owner.GetPhotonTeam() + " + " + PhotonNetwork.LocalPlayer.GetPhotonTeam());
-                    PhotonNetwork.Instantiate(impact_explosion.name, hit.point, Quaternion.identity);
-                    //"if()" is a good name of a book
-                    if ((hit.collider.tag == "AI_Player" || hit.collider.tag == "Player") && hit.collider.GetComponent<Team_Handler>().GetTeam() != player.GetComponent<Team_Handler>().GetTeam())
+                    RaycastHit hit = secondHit.Value;
+                    if (hit.distance <= range)
                     {
-                        Player_Health ph = hit.collider.GetComponent<Player_Health>();
-                        if (ph != null)
+                        PhotonNetwork.Instantiate(impact_explosion.name, hit.point, Quaternion.identity);
+
+                        // Example damage/target logic
+                        if ((hit.collider.CompareTag("AI_Player") || hit.collider.CompareTag("Player")) &&
+                            hit.collider.GetComponent<Team_Handler>().GetTeam() != parent.GetComponent<Team_Handler>().GetTeam())
                         {
-                            if (ph.current_health > 0)
-                            {
-
-                                if (ph.current_health - damage / 2 <= 0)
-                                {
-                                    PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
-                                    Transform oldpos = hit.collider.transform;
-                                    hit.collider.transform.position = new Vector3(9999, 9999, 9999);
-                                    SpawnCoin(hit.transform.gameObject, hit.point);
-                                    //ph.PlayerLastHit(pv.ViewID); //base this off of the gameObject, not ID
-                                    parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
-
-
-                                }
-                                ph.Remove_Health(damage / 2);
-                            }
-
-                        }
-                        else
-                        {
-
-                        }
-
-                    }
-                    else if ((hit.collider.tag == "AI_Head" || hit.collider.tag == "Player_Head") && hit.collider.GetComponentInParent<Team_Handler>().GetTeam() != player.GetComponent<Team_Handler>().GetTeam())
-                    {
-                        Player_Health ph = hit.collider.GetComponentInParent<Player_Health>();
-                        if (ph != null)
-                        {
-                            if (ph.current_health > 0)
+                            Player_Health ph = hit.collider.GetComponent<Player_Health>();
+                            if (ph != null && ph.current_health > 0)
                             {
                                 if (ph.current_health - (damage / 2) <= 0)
                                 {
                                     PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
-                                    Transform oldpos = hit.collider.transform;
                                     hit.collider.transform.position = new Vector3(9999, 9999, 9999);
-                                    SpawnCoin(hit.transform.parent.gameObject, hit.point);
-                                    //ph.PlayerLastHit(pv.ViewID);
+                                    SpawnCoin(hit.transform.gameObject, hit.point);
                                     parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
-
                                 }
                                 ph.Remove_Health(damage / 2);
                             }
                         }
-                        else
+                        else if ((hit.collider.CompareTag("AI_Head") || hit.collider.CompareTag("Player_Head")) &&
+                                 hit.collider.GetComponentInParent<Team_Handler>().GetTeam() != parent.GetComponent<Team_Handler>().GetTeam())
                         {
-                            Debug.Log("too short");
+                            Player_Health ph = hit.collider.GetComponentInParent<Player_Health>();
+                            if (ph != null && ph.current_health > 0)
+                            {
+                                if (ph.current_health - (damage / 2) <= 0)
+                                {
+                                    PhotonNetwork.Instantiate(death_effect.name, hit.point, Quaternion.identity);
+                                    hit.collider.transform.position = new Vector3(9999, 9999, 9999);
+                                    SpawnCoin(hit.transform.parent.gameObject, hit.point);
+                                    parent.GetComponentInParent<Super_Bar>().ChangeAmount(35);
+                                }
+                                ph.Remove_Health(damage / 2);
+                            }
                         }
-
-
                     }
-
                 }
-
             }
         }
     }
+
+
+    private RaycastHit? SphereCastIgnoreKinematic(Ray ray, float radius, float maxDistance, int layerMask = ~0)
+    {
+        // Grab all hits
+        RaycastHit[] hits = Physics.SphereCastAll(
+            ray,
+            radius,
+            maxDistance,
+            layerMask,
+            QueryTriggerInteraction.Ignore
+        );
+
+        if (hits.Length == 0)
+            return null;
+
+        // Sort by distance so we process the closest hits first
+        System.Array.Sort(hits, (h1, h2) => h1.distance.CompareTo(h2.distance));
+
+        // Loop through and ignore any that have a kinematic RB
+        foreach (var h in hits)
+        {
+            if (h.collider.isTrigger && (h.collider.tag != "Player_Head" || h.collider.tag != "AI_Head")) {
+                continue;
+            }
+            // If there's no rigidbody or it's not kinematic, this is our "real" hit
+            Rigidbody rb = h.rigidbody;
+            if (rb == null || rb.isKinematic == false)
+            {
+                return h;
+            }
+        }
+
+        // If all hits had kinematic rigidbodies, return null
+        return null;
+    }
+
 }
