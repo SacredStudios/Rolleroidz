a0ef69e171faa8886902f48cd83cfeac9b85d4aa
Jessie Coleman <97319486+SacredStudios@users.noreply.github.com>
2025-05-04 10:49:56 -0400

    Update PoisonWeapon.cs

--- Sk8troidz/Assets/Scripts/Weapon/PoisonWeapon.cs
+++ Sk8troidz/Assets/Scripts/Weapon/PoisonWeapon.cs
@@ -1,15 +1,45 @@
 using UnityEngine;
 using Photon.Pun;
+using System.Diagnostics;
 
 [CreateAssetMenu(fileName = "PoisonWeapon", menuName = "Scriptable Objects/PoisonWeapon")]
 public class PoisonWeapon : RangedWeapon
 {
     public float _poisonRadius = 5f;
     protected override float Radius => _poisonRadius;
-    public GameObject poison_effect;
-    public override void Shoot(GameObject parent, GameObject particlePos, GameObject explosionPos)
+    // PoisonWeapon.cs
+using UnityEngine;
+using Photon.Pun;
+
+[CreateAssetMenu(fileName = "PoisonWeapon", menuName = "Scriptable Objects/PoisonWeapon")]
+public class PoisonWeapon : RangedWeapon
+{
+    [Header("Poison Settings")]
+    [SerializeField] private float poisonDuration = 5f; // seconds
+
+    protected override void ApplyDamage(RaycastHit hit, GameObject parent)
     {
-        base.Shoot(parent, particlePos, explosionPos);
-        PhotonNetwork.Instantiate(poison_effect.name, explosionPos.transform.position, explosionPos.transform.rotation);
+        // (1) do all the normal damage/death logic
+        base.ApplyDamage(hit, parent);
+
+        // (2) spawn a poison VFX on the target
+        if (particle_explosion != null)
+        {
+            PhotonNetwork.Instantiate(
+                particle_explosion.name,
+                hit.point,
+                Quaternion.identity
+            );
+        }
+
+        // (3) apply poison status over time if you have it on Player_Health:
+        var ph = hit.collider.GetComponentInParent<Player_Health>();
+        if (ph != null)
+        {
+            // ph.ApplyPoison(poisonDuration);
+            Debug.Log("test");
+        }
     }
+}
+
 }
